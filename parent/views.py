c623e34a84a43faaef43a707fc4bd203b409c99b
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2022-03-30 19:53:38 +0545

    make child subjects POST

--- parent/views.py
+++ parent/views.py
@@ -2,6 +2,7 @@ import json
 
 from django.contrib.auth.models import User
 from django.http import HttpRequest, HttpResponse
+
 from .models import *
 
 
@@ -15,17 +16,20 @@ def get_children(request: HttpRequest) -> HttpResponse:
 
 
 def get_child_subjects(request: HttpRequest) -> HttpResponse:
-    if request.user.is_authenticated:
-        parent: User = request.user
-        children: list = parent.child_set.all()
-        subjects: dict[str, list[str]] = {}
-        for child in children:
-            current_child: QuerySet = child.childsubject_set.all()
-            cs: ChildSubject
-            for c in current_child:
-                if str(c.child_name) in subjects:
-                    subjects[str(c.child_name)] += [c.subject]
-                else:
-                    subjects[str(c.child_name)] = [c.subject]
-        return HttpResponse(json.dumps(subjects, default=str))
-    return HttpResponse('No current user found', status=401)
+    if request.method == 'POST':
+        import json
+        parent: User = User.objects.get(userId=json.loads(request.body.decode())['uid'])
+        if parent is not None:
+            children: list = parent.child_set.all()
+            subjects: dict[str, list[str]] = {}
+            for child in children:
+                current_child: QuerySet = child.childsubject_set.all()
+                cs: ChildSubject
+                for c in current_child:
+                    if str(c.child_name) in subjects:
+                        subjects[str(c.child_name)] += [c.subject]
+                    else:
+                        subjects[str(c.child_name)] = [c.subject]
+            return HttpResponse(json.dumps(subjects, default=str))
+        return HttpResponse('No such user found', status=401)
+    return HttpResponse('Bad reuqest. Only POST allowed', status=401)
