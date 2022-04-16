aec8c9c384ac3d8bc208945f0424df50b2f0a3c8
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2022-04-16 20:34:14 +0545

    fix csrf

--- feedback/views.py
+++ feedback/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
-
 # Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+
 from feedback.models import Feedback
 
 
+@csrf_exempt
 def post_feedback(request):
     if request.method == 'POST':
         import json
@@ -11,4 +13,5 @@ def post_feedback(request):
         title = post_data['title']
         description = post_data['description']
         Feedback.objects.create(title=title, description=description)
+        return HttpResponse('Posted')
     return HttpResponse('Only POST allowed', status=401)
